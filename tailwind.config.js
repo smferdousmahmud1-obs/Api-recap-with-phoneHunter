eb70a996b3acf3dafc10201369860a31d03aef80
smferdousmahmud1-obs <smferdousmahmud1@gmail.com>
2026-01-20 20:33:14 +0600

    create files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+    module.exports = {
+      content: [
+        "./index.html", // Or your main HTML file
+        "./src/**/*.{js,ts,jsx,tsx,vue}", // Adjust based on your project structure and file types
+      ],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    }
