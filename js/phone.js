98d747d92bbdc967f9360f23002253dfae8e5881
smferdousmahmud1-obs <smferdousmahmud1@gmail.com>
2026-01-20 23:05:40 +0600

    all

--- js/phone.js
+++ js/phone.js
@@ -7,10 +7,28 @@ const loadPhone = async () =>{
 }
 const displayPhone = phones => {
     //console.log(phones);
+
+    const phoneContainer = document.getElementById('Phone-container');
+
     phones.forEach(phone =>{
         console.log(phone);
-    }
-     )
+        // 2 Create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
+        // 3 set inner html
+        phoneCard.innerHTML = `
+        <figure><img src="${phone.image}" alt="Shoes" /></figure>
+                            <div class="card-body">
+                            <h2 class="card-title">${phone.phone_name}</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                            </div>
+                            </div>
+        `
+        // 4 append child 
+        phoneContainer.appendChild(phoneCard);
+    })
 }
 
 
